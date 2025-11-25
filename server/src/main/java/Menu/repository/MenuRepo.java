ddd95c1a159fc75a2f70ed18a72b67c8b70ba22a
Ishmaam Iftekhar Khan <ishmaam@iut-dhaka.edu>
2025-11-25 10:44:36 +0600

    add: menu and order repositories created

---
+++ server/src/main/java/Menu/repository/MenuRepo.java
@@ -0,0 +1,15 @@
+package Menu.repository;
+
+import Menu.domain.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface MenuRepo extends JpaRepository<Menu, UUID> {
+    List<Menu> findByCategory(String category);
+
+    boolean ExistsByName(String name);
+}
---
+++ server/src/main/java/Orders/repository/OrderRepo.java
@@ -0,0 +1,14 @@
+package Orders.repository;
+
+import Orders.domain.Order;
+import Orders.enums.OrderStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface OrderRepo extends JpaRepository<Order, UUID> {
+    List<Order> getOrdersByUser(UUID userId);
+
+    List<Order> findOrdersByStatus(OrderStatus status);
+}
