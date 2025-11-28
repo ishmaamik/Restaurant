c928db4cc6931b0a0f61c2315f2e496e5170ff52
Ishmaam Iftekhar Khan <ishmaam@iut-dhaka.edu>
2025-11-28 18:49:18 +0600

    add: User Details and User Details Service

---
+++ server/src/main/java/Security/services/AppUserDetailsService.java
@@ -0,0 +1,22 @@
+package Security.services;
+
+import Security.domain.AppUserDetails;
+import Users.domain.User;
+import Users.services.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AppUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    public AppUserDetailsService(UserService userService){
+        this.userService= userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user= userService.getUserByUsername(username);
+        return new AppUserDetails(user);
+    }
+}
