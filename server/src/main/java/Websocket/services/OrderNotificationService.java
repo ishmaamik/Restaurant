c6353187500108ce8115971e74dc96950443807b
Ishmaam Iftekhar Khan <ishmaam@iut-dhaka.edu>
2025-12-10 15:59:30 +0600

    add: started Order Notification Service for notifying users about the orders

---
+++ server/src/main/java/Websocket/services/OrderNotificationService.java
@@ -0,0 +1,21 @@
+package Websocket.services;
+
+import Orders.DTOs.OrderCustomerDTO;
+import Orders.domain.Order;
+import Orders.mappers.OrderMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
+public class OrderNotificationService {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+    @Autowired
+    private OrderMapper orderMapper;
+
+    //NOTIFIES KITCHEN COOKS ABOUT THE ORDERS
+    public void notifyOrderToKitchen(Order order){
+        OrderCustomerDTO orderCustomerDTO= orderMapper.toCustomerOrderDTO(order);
+        simpMessagingTemplate.convertAndSend("/topic/kitchen", orderCustomerDTO);
+    }
+}
